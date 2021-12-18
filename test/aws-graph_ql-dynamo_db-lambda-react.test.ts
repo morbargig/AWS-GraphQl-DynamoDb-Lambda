b04e2922fe384c64c3ec424718e24ba8ddfb0963
mor bargig <mor@ideo-digital.com>
2021-12-19 01:46:46 +0200

    Initial commit

---
+++ test/aws-graph_ql-dynamo_db-lambda-react.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsGraphQlDynamoDbLambdaReact from '../lib/aws-graph_ql-dynamo_db-lambda-react-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-graph_ql-dynamo_db-lambda-react-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsGraphQlDynamoDbLambdaReact.AwsGraphQlDynamoDbLambdaReactStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
